5eb752db29e6a2bcbec0d2cebce96ca0ce109ebd
toblaroni <tobyhutchinson@gmail.com>
2023-08-09 11:00:14 +0100

    Started adding my own server code for register

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ import express from 'express';
 import { DwnHelper } from './src/utils/dwn.js';
 import { initAgent } from './src/utils/agent.js';
 import cors from 'cors'
+
+// Connection to database
+import { CosmosClient } from "@azure/cosmos";
+
+const endpoint = process.env.COSMOS_ENDPOINT;
+const key = process.env.COSMOS_KEY;
+const client = new CosmosClient({ endpoint, key });
+
 // import {
 //     AriesRFCsPlugin,
 //     MESSAGE_TYPES_0453,
@@ -11,6 +19,7 @@ import cors from 'cors'
 const app = express();
 const port = 3001;
 app.use(express.json());
+app.use(express.urlencoded({ extended: false })); // Body parse middleware
 app.use(cors())
 
 const dwn = new DwnHelper(process.env.DWN_URL)
@@ -24,99 +33,104 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// Register
+app.get('/setup', async (req, res) => {
+  // this is where we need to merge the sign up code. the keys variable holds the data that we need to use to sign/encrypt messages
+  let response = await initAgent('test')
+  identifier = response.identifier
+  keys = response.decodedKeys
+  agent = response.agent
 
-app.get('/setup', async (req,res) => {
-    // this is where we need to merge the sign up code. the keys variable holds the data that we need to use to sign/encrypt messages
-    let response = await initAgent('test')
-    identifier = response.identifier
-    keys = response.decodedKeys
-    agent = response.agent
-    // console.log(agent)
-    await dwn.init()
-    res.send(agent)
+  // Connect to container
+  const { database } = await client.databases.createIfNotExists({ id: "itemsDB" });
+  const { container } = await database.containers.createIfNotExists({ id: "wallet-users" });
+
+  // console.log(agent)
+  await dwn.init()
+  res.send(agent)
 });
 
-app.post('/dwn/init', async (req,res) => {
-    const { protocol, definition } = req.body
-    const message = await dwn.createProtocol(protocol, definition, keys, identifier)
-    const result = await dwn.send(message);
-    res.send(result)
+app.post('/dwn/init', async (req, res) => {
+  const { protocol, definition } = req.body
+  const message = await dwn.createProtocol(protocol, definition, keys, identifier)
+  const result = await dwn.send(message);
+  res.send(result)
 
 })
 
-app.post('/dwn/read', async (req,res) => {
-    const { target_did, protocol, schema } = req.body
-    let msg = await dwn.createRecordsQuery(protocol, schema, keys, target_did)
-    let response = await dwn.send(msg)
-    let data = dwn.decodeRecordsQueryData(response)
-    res.send(data)
+app.post('/dwn/read', async (req, res) => {
+  const { target_did, protocol, schema } = req.body
+  let msg = await dwn.createRecordsQuery(protocol, schema, keys, target_did)
+  let response = await dwn.send(msg)
+  let data = dwn.decodeRecordsQueryData(response)
+  res.send(data)
 })
 
-app.post('/dwn/write', async (req,res) => {
-    const { target_did, protocol, schema, data } = req.body
-    const msg = await dwn.createRecordsWrite(protocol, schema, data, keys, target_did)
-    const response = await dwn.send(msg);
-    res.send(response)
+app.post('/dwn/write', async (req, res) => {
+  const { target_did, protocol, schema, data } = req.body
+  const msg = await dwn.createRecordsWrite(protocol, schema, data, keys, target_did)
+  const response = await dwn.send(msg);
+  res.send(response)
 })
 
-app.post('/perms/request', (req,res) => {
+app.post('/perms/request', (req, res) => {
 
 })
 
-app.post('/perms/grant', (req,res) => {
-    
+app.post('/perms/grant', (req, res) => {
+
 })
 
-app.post('/perms/read', (req,res) => {
-    
+app.post('/perms/read', (req, res) => {
+
 })
 
-app.post('/perms/read/reject', (req,res) => {
-    
+app.post('/perms/read/reject', (req, res) => {
+
 })
 
-app.post('/perms/read/grant', (req,res) => {
-    
+app.post('/perms/read/grant', (req, res) => {
+
 })
 
 
 // FOR THE FOLLOWING FUNCTIONALITY AROUND VCS, REFERENCE
 // https://github.com/spherity/aries-rfcs-veramo-plugin#sending-messages
 
-app.get('/vcs', (req,res) => {
-    // read credential in the database
+app.get('/vcs', (req, res) => {
+  // read credential in the database
 })
 
-app.post('/vcs/issue', async (req,res) => {
-    const { target_did, credential } = req.body
+app.post('/vcs/issue', async (req, res) => {
+  const { target_did, credential } = req.body
 
-    let response = await ariesPlugin.send0453(
-        {
-          to: target_did,
-          from: identifier,
-          type: MESSAGE_TYPES_0453,
-          packingType: DIDCommMessagePacking,
-          message: {
-            '@type': MESSAGE_TYPES_0453,
-            credentialBody: credential
-          }
-        },
-        { agent }
-      );
+  let response = await ariesPlugin.send0453(
+    {
+      to: target_did,
+      from: identifier,
+      type: MESSAGE_TYPES_0453,
+      packingType: DIDCommMessagePacking,
+      message: {
+        '@type': MESSAGE_TYPES_0453,
+        credentialBody: credential
+      }
+    },
+    { agent }
+  );
 
-    res.send(response)
+  res.send(response)
 })
 
-app.post('/vcs/request', (req,res) => {
-    
+app.post('/vcs/request', (req, res) => {
+
 })
 
-app.post('/vcs/present', (req,res) => {
-    
+app.post('/vcs/present', (req, res) => {
+
 })
 
-app.post('/vcs/verify', (req,res) => {
-    
+app.post('/vcs/verify', (req, res) => {
+
 })
 
 app.listen(port, () => {
